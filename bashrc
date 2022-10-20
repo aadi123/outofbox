a099d6b7e49d1c217074fe4003e80b68cd883f2b
Aaditya Murthy <aadi.murthyananth@gmail.com>
2022-10-20 16:10:28 -0500

    Added stuff

--- bashrc
+++ bashrc
@@ -28,3 +28,4 @@ unset rc
 
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 eval "$(oh-my-posh init bash --config $(brew --prefix oh-my-posh)/themes/jandedobbeleer.omp.json)"
+export PATH="~/.emacs.d/bin:$PATH"
---
+++ install.sh
@@ -0,0 +1,6 @@
+# Install homebrew
+NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Install other stuff
+brew install gcc
+brew install jandedobbeleer/oh-my-posh/oh-my-posh
