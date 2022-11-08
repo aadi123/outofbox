9a3f115f3dfe1f75ed624e20955d52839a219fd5
Aaditya Murthy <aadi.murthyananth@gmail.com>
2022-11-08 15:27:25 -0600

    Added hard links

--- doom/custom.el
+++ doom/custom.el
@@ -3,7 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files '("/home/aaditya/org/daily-journal/10-20-2022.org")))
+ '(org-agenda-files
+   '("/home/aaditya/org/daily-journal/11-11-2022.org" "/home/aaditya/org/daily-journal/11-08-2022.org")))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
