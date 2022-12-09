36dcc1fae2687b0bfaff22fbb8361b7c9d184289
Aaditya Murthy <aadi.murthyananth@gmail.com>
2022-12-09 10:51:27 -0600

    Changed shit

--- install.sh
+++
@@ -1,6 +0,0 @@
-# Install homebrew
-NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-# Install other stuff
-brew install gcc
-brew install jandedobbeleer/oh-my-posh/oh-my-posh
