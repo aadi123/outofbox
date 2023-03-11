309db2fa07d518e4bb96d5b8fc3f808b42a81d18
Aaditya Murthy <aadi.murthyananth@gmail.com>
2023-03-11 15:26:57 -0600

    Update

--- doom/custom.el
+++ doom/custom.el
@@ -3,6 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-agenda-files
+   '("~/org/second-brain/20230311115555-todos.org" "/home/aaditya/org/second-brain/daily/2023-03-03.org" "/home/aaditya/org/second-brain/daily/2023-03-04.org" "/home/aaditya/org/second-brain/daily/2023-03-05.org" "/home/aaditya/org/second-brain/daily/2023-03-06.org" "/home/aaditya/org/second-brain/daily/2023-03-07.org" "/home/aaditya/org/second-brain/daily/2023-03-08.org" "/home/aaditya/org/second-brain/daily/2023-03-09.org" "/home/aaditya/org/second-brain/daily/2023-03-10.org" "/home/aaditya/org/second-brain/daily/2023-03-11.org"))
  '(package-selected-packages '(org-contrib)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
