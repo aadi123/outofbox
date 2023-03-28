6f511a238b6f6fe51d8621b4961133341052799b
Aaditya Murthy <aadi.murthyananth@gmail.com>
2023-03-28 08:08:27 -0500

    Edited date format for org-journal

--- doom/custom.el
+++ doom/custom.el
@@ -3,7 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files '("/home/aaditya/Dropbox/org/journal/2023-03-25.org"))
+ '(org-agenda-files
+   '("/home/aaditya/Dropbox/org/journal/2023-06-25.org" "/home/aaditya/Dropbox/org/journal/2023-03-28.org"))
  '(package-selected-packages '(org-contrib)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
