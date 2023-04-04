ca61e7c720305fd67e497fc7e8a7a733b8990ebe
Aaditya Murthy <aadi.murthyananth@gmail.com>
2023-04-04 14:03:29 -0500

    Update

--- doom/config.el
+++ doom/config.el
@@ -97,6 +97,10 @@
   (setq org-journal-file-format "%Y-%m-%d.org")
   (setq org-journal-time-format "%I:%M %p"))
 
+(after! org-hyperscheduler
+  (setq org-hyperscheduler-exclude-from-org-roam t)
+  (setq org-hyperscheduler-readonly-mode nil))
+
 (use-package! websocket
     :after org-roam)
 
--- doom/packages.el
+++ doom/packages.el
@@ -49,7 +49,11 @@
 ;; ...Or *all* packages (NOT RECOMMENDED; will likely break things)
 ;(unpin! t)
 (unpin! org-roam)
-(unpin! org-gcal)
 (package! org-roam-ui)
 (package! copilot
   :recipe (:host github :repo "zerolfx/copilot.el" :files ("*.el" "dist")))
+(package! org-hyperscheduler
+  :recipe (:repo "dmitrym0/org-hyperscheduler"
+    :host github
+    :type git
+    :files ("*")))
