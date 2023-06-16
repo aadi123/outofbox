f52c7fa157472036eb5af13a711093a5720a2e0b
Aaditya Murthy <aadi.murthyananth@gmail.com>
2023-06-15 21:53:28 -0500

    Update

--- doom/custom.el
+++ doom/custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(org-agenda-files
-   '("/home/aaditya/Dropbox/org/inbox.org" "/home/aaditya/Dropbox/org/gcal.org" "/home/aaditya/Dropbox/org/todo.org" "/home/aaditya/Dropbox/org/journal/2023-06-25.org" "/home/aaditya/Dropbox/org/journal/2023-05-29.org"))
+   '("/home/aaditya/Dropbox/org/inbox.org" "/home/aaditya/Dropbox/org/gcal.org" "/home/aaditya/Dropbox/org/todo.org" "/home/aaditya/Dropbox/org/journal/2023-06-25.org" "/home/aaditya/Dropbox/org/journal/2023-06-15.org"))
  '(package-selected-packages '(org-contrib)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
--- fish/config.fish
+++ fish/config.fish
@@ -5,6 +5,9 @@ and not set -q TMUX
 end
 set -gx ORG_DIR /home/aaditya/Dropbox/org
 set -gx LEDGER_FILE $ORG_DIR/second-brain/time-money-tracking/finances/2023.journal
+set -gx GOPATH $HOME/go
+fish_add_path $HOME/.config/emacs/bin
+fish_add_path $GOPATH/bin
 set cmd (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 eval $cmd
 starship init fish | source
